e539a5f33be5e10c7ad00906ec191a66519cfef1
KevinBozic <bozic.kevin@gmail.com>
2016-02-15 13:46:41 -0500

    Sorted by last name

--- src/People.java
+++ src/People.java
@@ -1,5 +1,55 @@
-public class People {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
 
+public class People {
+    public static void main(String[] args) throws FileNotFoundException {
+        HashMap<String, ArrayList<Person>> countryNameMap = new HashMap<>();
+
+        String fileContents = readFile("people.csv");
+        String[] lines = fileContents.split("\n");
+        String s = new String();
+        int i =0;
+
+        for (String line : lines) {
+            if (i != 0) {
+
+                String[] columns = line.split("\\,");
+                String id = columns[0];
+                String firstName = columns[1];
+                String lastName = columns[2];
+                String email = columns[3];
+                String country = columns[4];
+                String ipAddress = columns[5];
+                Person p = new Person(id, firstName, lastName, email, country, ipAddress);
+                if (!countryNameMap.containsKey(country)) {
+                    countryNameMap.put(country, new ArrayList<Person>());
+                }
+                countryNameMap.get(country).add(p); //then we add our country object "c" to the arraylist in the hashmap
+                Collections.sort(countryNameMap.get(country));
+            }
+            i++;
+        }
+
+        for (String country : countryNameMap.keySet()) {
+            for (Person p : countryNameMap.get(country)) {
+                s += String.format("%s %s from %s\n", p.firstName, p.lastName, p.country);
+            }
+
+        }
+        //System.out.println(countryNameMap.toString());
+        System.out.println(s);
+    }
+
+    static String readFile(String fileName) throws FileNotFoundException {
+        File f = new File(fileName);
+        Scanner fileScanner = new Scanner(f);
+        fileScanner.useDelimiter("\\Z");
+        String fileContents;
+        fileContents = fileScanner.next();
+        return fileContents;
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,83 @@
+public class Person implements Comparable { // This class contains the fields...
+    String id;
+    String firstName;
+    String lastName;
+    String email;
+    String country;
+    String ipAddress;
+
+    public Person(String id, String firstName, String lastName, String email, String country, String ipAddress) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.country = country;
+        this.ipAddress = ipAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", country='" + country + '\'' +
+                ", ipAddress='" + ipAddress + '\'' +
+                '}'+"\n";
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Person p = (Person) o; /////// WHAT THE HELL IS THIS????
+        return lastName.compareTo(p.lastName);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+}
